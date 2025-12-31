391a0b16f29122d7f27e54ab41e3ae29ec198d76
alejs-dev <alexmardarilel@gmail.com>
2025-12-31 18:03:13 +0100

    added links to exercises

---
+++ index.js
@@ -0,0 +1,67 @@
+function onExcerciceClick(sExcerciseName) {
+    let url = "";
+    switch (sExcerciseName) {
+        case "Bench Press":
+            url = "www.youtube.com/watch?v=SCVCLChPQFY";
+            window.open(url, "_blank").focus();
+            break; 
+        case "Chest Press":
+            url = "www.youtube.com/watch?v=n8TOta_pfr4";
+            window.open(url, "_blank").focus();
+            break; 
+        case "Shoulder Press": 
+            url = "www.youtube.com/watch?v=y_NJ7mZYM3I";
+            window.open(url, "_blank").focus();
+            break; 
+        case "Lateral Raises": 
+            url = "www.youtube.com/watch?v=gjXX8g-VPXI"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Triceps Pushdown": 
+            url = "www.youtube.com/shorts/Rc7-euA8FDI"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Cross Body Triceps Extensions": 
+            url = "www.youtube.com/shorts/HCc3kDzTyTQ"; 
+            window.open(url, "_blank").focus();
+            break; 
+
+        case "Lat Pulldown": 
+            url = "www.youtube.com/watch?v=trZQjegcRx0"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Rows": 
+            url = "www.youtube.com/shorts/TXPefiUFMvw"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Lat Rows": 
+            url = "www.youtube.com/watch?v=GZbfZ033f74"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Cable Hammer Curls": 
+            url = "www.youtube.com/watch?v=V8AR3SGzboU"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Preacher Curls": 
+            url = "www.youtube.com/shorts/dXGoere0Hxo"; 
+            window.open(url, "_blank").focus();
+            break; 
+
+        case "Hack-Squats": 
+            url = "www.youtube.com/shorts/g9i05umL5vc"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Leg Extensions": 
+            url = "www.youtube.com/shorts/uM86QE59Tgc"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Leg Curls": 
+            url = "www.youtube.com/shorts/FMCq0hT3KRU"; 
+            window.open(url, "_blank").focus();
+            break; 
+        case "Calf Raises": 
+            url = "www.youtube.com/watch?v=pz66Bw6HJ4s"; 
+            window.open(url, "_blank").focus();
+            break; 
+    }    
+}
